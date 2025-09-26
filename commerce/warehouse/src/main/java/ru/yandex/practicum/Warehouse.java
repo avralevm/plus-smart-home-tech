3a841d111ed2811f8c4742bd2fe6464728eb9e48
Авралев Максим <avralevm@yandex.ru>
2025-09-26 12:51:27 +0500

    Добавлен basePackages для FeignClient

--- commerce/warehouse/src/main/java/ru/yandex/practicum/Warehouse.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/Warehouse.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "ru.yandex.practicum.client")
 public class Warehouse {
     public static void main(String[] args) {
         SpringApplication.run(Warehouse.class);
